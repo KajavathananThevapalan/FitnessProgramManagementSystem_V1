9b9b37270956bc6f87db75ef4dec2f61d2935470
Thevapalan K. <UT01360@unicomtic.com>
2024-10-11 10:36:47 +0530

    Q5_V1

---
+++ FitnessProgramManagementSystem/FitnessProgramManager .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class FitnessProgramManager
+    {
+        public List<FitnessProgram> FitnessProgramList;
+
+        public FitnessProgramManager() {
+            
+            FitnessProgramList = new List<FitnessProgram>();
+
+        }
+
+        public static void CreateFitnessProgram()
+        {
+
+        }
+
+        public static void ReadFitnessPrograms()
+        {
+
+        }
+
+        public static void UpdateFitnessProgram()
+        {
+
+        }
+
+        public static void DeleteFitnessProgram()
+        {
+
+        }
+    }
+}
--- FitnessProgramManagementSystem/Program.cs
+++ FitnessProgramManagementSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FitnessProgramManagementSystem
 {
@@ -10,9 +11,51 @@ namespace FitnessProgramManagementSystem
     {
         static void Main(string[] args)
         {
-            var FitnessProgram1 = new FitnessProgram("1", "Weight Lifting", "4 months", "1000.00");
-            Console.WriteLine(FitnessProgram1.ToString());
+            //var FitnessProgram1 = new FitnessProgram("1", "Weight Lifting", "4 months", "1000.00");
+            //Console.WriteLine(FitnessProgram1.ToString());
+            
+
+            Menu();
             Console.ReadLine();
+
+        }
+
+        public static void Menu()
+        {
+            var manager = new FitnessProgramManager();
+
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("----------------------FitnessProgram Management System: -------------------------");
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("1. Add a FitnessProgram");
+            Console.WriteLine("2. View All FitnessPrograms");
+            Console.WriteLine("3. Update a FitnessProgram");
+            Console.WriteLine("4. Delete a FitnessProgram");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("Choose an option:");
+            var UserInput = Console.ReadLine();
+
+            switch (UserInput) {
+                case "1":
+                    manager.CreateFitnessProgram();
+                    break;
+                case "2":
+                    manager.ReadFitnessPrograms();
+                    break;
+                case "3":
+                    manager.UpdateFitnessProgram();
+                    break;
+                case "4":
+                    manager.DeleteFitnessProgram();
+                    break;
+                case "5":
+                    Console.WriteLine("Thank You!!! Come Again!!!");
+                    return;
+                default:
+                    Menu();
+                    break;
+
+            }
         }
     }
 }
